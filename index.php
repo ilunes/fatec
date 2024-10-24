e9c0aaa81d5517660c26bde7d98e45b1c7ffa90e
ilunes <55817007+ilunes@users.noreply.github.com>
2024-10-24 08:58:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang ="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <!-- <link rel="stylesheet" href="estilo.css"> -->
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>
+            Calculadora
+        </title>
+        
+    </head>
+    <body>
+        <?php 
+        echo "Olá mundo! <br> ";
+        echo "Olá mundo! <br> ";
+
+        print("Hello World! <br> ");
+        print("Hello World! <br> ");
+
+        $vetor = array('Pa','Gs','Fd');
+
+        var_dump($vetor);
+        echo "<br>";
+        print_r($vetor);
+
+        ?>
+    </body>
+</html>
